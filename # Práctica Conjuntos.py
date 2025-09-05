83e7abd17b777c0e9c778921acfffe572e267544
Crizzroot <crizzroot@gmail.com>
2025-09-05 07:06:14 -0300

    Primer commit - ejercicios de conjuntos

---
+++ # Práctica Conjuntos.py
@@ -0,0 +1,38 @@
+# Conjuntos
+'''A = {"manzana", "banana", "pera"}
+B = {"banana", "naranja", "uva"}
+
+# 1 Unión
+union = A | B
+print("Frutas en A o en B (o en ambos):", union)
+
+A = {"Breaking Bad", "Friends", "The Office"}
+B = {"Friends", "Dark", "The Office"}
+
+# 2 Intersección
+interseccion = A & B
+print("Series que miramos ambos:", interseccion)
+
+A = {"FIFA", "Minecraft", "GTA"}
+B = {"GTA", "Mario Kart", "Zelda"}
+
+# 3 Diferencia Simétrica
+diferencia_simetrica = A ^ B
+print("Juegos exclusivos de cada uno:", diferencia_simetrica)
+
+A = {"harina", "queso", "tomate"}
+B = {"harina", "queso", "tomate", "levadura", "sal", "aceite"}
+
+# 4 Subconjunto
+es_subconjunto = A.issubset(B)
+print("¿Puedo hacer la pizza con lo que tengo en la heladera?", es_subconjunto)
+
+# 5 Cantidad de elementos
+A = {"Avatar", "Titanic", "Inception", "Interstellar"}
+
+cantidad = len(A)
+print("Cantidad de películas vistas:", cantidad)'''
+
+
+
+
